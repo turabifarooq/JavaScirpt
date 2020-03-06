ee6cadbfc0aa5943d2041c262b05c2cff1ba18d4
hirawaseem <amjadalifarooq7@gamil.com>
2020-03-06 16:26:53 +0500

    java

--- java.js
+++ java.js
@@ -1,34 +1,20 @@
+var firstValue = prompt("Enter number:")
+var lastValue = prompt("Enter last number:")
+alert(firstValue * lastValue)
 alert("Calculator!")
 var firstNum = prompt("Enter first number:" , "");
 var lastNum = prompt("Enter last number:" , "");
 
 var bodmas = prompt("What do you want ? 1.Multiplication 2.Devide 3.Addition 4.Substraction" , "");
-if(bodmas === "1"){
+if(bodmas == "1"){
     alert(firstNum * lastNum)
 };
 if(bodmas === "2"){
     alert(firstNum / lastNum)
 };
 if(bodmas === "3"){
-    alert(firstNum + lastNum)
+    alert((+firstNum) + (+lastNum))
 };
 if(bodmas === "4"){
     alert(firstNum - lastNum)
 };
-
-
-// Percentage
-
-var perentage = prompt("Write your percentage:", "")
-if(perentage > 79){
-    alert("You got A1+")
-}
-else if(perentage > 69 && perentage < 80){
-    alert("You got A1")
-}
-else if(perentage > 59 && perentage < 70){
-    alert("You got B")
-}
-else{
-    alert("You are failed")
-}
