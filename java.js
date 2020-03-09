c189fccd21ff628fb6e91a9368c75b06e60e5ba8
hirawaseem <amjadalifarooq7@gamil.com>
2020-03-09 15:54:37 +0500

    java

--- java.js
+++ java.js
@@ -1,20 +1,38 @@
-var firstValue = prompt("Enter number:")
-var lastValue = prompt("Enter last number:")
-alert(firstValue * lastValue)
-alert("Calculator!")
-var firstNum = prompt("Enter first number:" , "");
-var lastNum = prompt("Enter last number:" , "");
+// var firstValue = prompt("Enter number:")
+// var lastValue = prompt("Enter last number:")
+// alert(firstValue * lastValue)
+// alert("Calculator!")
+// var firstNum = prompt("Enter first number:" , "");
+// var lastNum = prompt("Enter last number:" , "");
 
-var bodmas = prompt("What do you want ? 1.Multiplication 2.Devide 3.Addition 4.Substraction" , "");
-if(bodmas == "1"){
-    alert(firstNum * lastNum)
-};
-if(bodmas === "2"){
-    alert(firstNum / lastNum)
-};
-if(bodmas === "3"){
-    alert((+firstNum) + (+lastNum))
-};
-if(bodmas === "4"){
-    alert(firstNum - lastNum)
-};
+// var bodmas = prompt("What do you want ? 1.Multiplication 2.Devide 3.Addition 4.Substraction" , "");
+// if(bodmas == "1"){
+//     alert(firstNum * lastNum)
+// };
+// if(bodmas === "2"){
+//     alert(firstNum / lastNum)
+// };
+// if(bodmas === "3"){
+//     alert((+firstNum) + (+lastNum))
+// };
+// if(bodmas === "4"){
+//     alert(firstNum - lastNum)
+// };
+
+
+var itemsList = ["Samose" , "Roll" , "Sandwitch" , "Namak Pare" , "Baloo Shae"];
+// alert(itemsList [2] + itemsList [0]);
+console.log(itemsList);
+itemsList[5] = "Golgappy"
+console.log(itemsList)
+itemsList.push("Zinger Burger")
+console.log(itemsList)
+itemsList.pop()
+itemsList.pop()
+console.log(itemsList)
+itemsList.shift()
+console.log(itemsList)
+itemsList.unshift("Qeema Samosa")
+console.log(itemsList)
+itemsList.splice(3 , 4 ,"Chicken Samosa");
+console.log(itemsList);
