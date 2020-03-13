20b586e07786789c57c24642bfb6416c88b9a054
hirawaseem <amjadalifarooq7@gamil.com>
2020-03-13 16:26:12 +0500

    java

--- java.js
+++ java.js
@@ -20,19 +20,32 @@
 // };
 
 
-var itemsList = ["Samose" , "Roll" , "Sandwitch" , "Namak Pare" , "Baloo Shae"];
-// alert(itemsList [2] + itemsList [0]);
-console.log(itemsList);
-itemsList[5] = "Golgappy"
-console.log(itemsList)
-itemsList.push("Zinger Burger")
-console.log(itemsList)
-itemsList.pop()
-itemsList.pop()
-console.log(itemsList)
-itemsList.shift()
-console.log(itemsList)
-itemsList.unshift("Qeema Samosa")
-console.log(itemsList)
-itemsList.splice(3 , 4 ,"Chicken Samosa");
-console.log(itemsList);
+// // alert(itemsList [2] + itemsList [0]);
+// console.log(itemsList);
+// itemsList[5] = "Golgappy"
+// console.log(itemsList)
+// itemsList.push("Zinger Burger")
+// console.log(itemsList)
+// itemsList.pop()
+// itemsList.pop()
+// console.log(itemsList)
+// itemsList.shift()
+// console.log(itemsList)
+// itemsList.unshift("Qeema Samosa")
+// console.log(itemsList)
+// itemsList.splice(3 , 4 ,"Chicken Samosa");
+// console.log(itemsList);
+// var itemsList2 = ["Turabi" , "Farooq" , "Amjad"];
+// console.log(itemsList2);
+// document.write("<b>Turabi Bin<br /> Farooq</b>");
+// itemsList2.push("Pak")
+// console.log(itemsList2)
+// itemsList2.splice(1 , 2 , "Amjads");
+// console.log(itemsList2)
+var bhook = prompt("item Ka Name Likho:")
+var itemsList = ["Samose" , "Roll" , "Sandwitch" , "Namak Pare" , "Baloo Shae" , "Papri" , "Chatni" , "Wonton" , "Cheese Roll" , "Chicken Roll"];
+for(var i = itemsList.length-1; i >= 0; i--) {
+    if(bhook === itemsList[i]){
+        alert(i)
+    }
+}
